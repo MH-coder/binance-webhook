d54fe482f75b05e66b3891c645b8c8cc81b27dec
Muhammad Hassan <mhassan786000@gmail.com>
2024-03-20 17:25:47 +0400

    add logs for debugging

--- src/utils/apiResponse.ts
+++ src/utils/apiResponse.ts
@@ -2,6 +2,18 @@ import { ApiResponseInterface } from '../interface';
 
 const apiResponse = (params: ApiResponseInterface) => {
   const { res, message = 'OK', code = 200, err, errors, success = true, data } = params;
+
+  console.log({
+    success: success,
+    status: success,
+    statusCode: code,
+    message: code === 500 ? 'Request Failed' : message,
+    errors,
+    stack: process.env.NODE_ENV === 'development' ? err?.stack : null,
+    data,
+  });
+  console.log('\n--------------------------\n');
+
   return res.status(code).json({
     success: success,
     status: success,
